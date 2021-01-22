e42907097857fc81278a4c5ac6b8dbe7fdab74a2
viitas96 <climav96@gmail.com>
2021-01-22 12:37:15 +0200

    ev22

---
+++ evaluare_2/app.js
@@ -0,0 +1,29 @@
+// //7.1 Задача №1 - событие загрузки документа и обращение к элементам DOM
+// //Сразу после открытия документа, установить красный цвет фона страницы.
+// //(задачи 1-4 выполнить в одном документе)
+// document.body.style.backgroundColor = "red";
+// //7.2 Задача №2 - событие загрузки документа и обращение к элементам DOM
+// //Через 3 секунды после загрузки страницы, установить цвет фона в зеленый.
+// setTimeout(() => document.body.style.background = "green", 3000);
+// //Задача №3 - событие click 
+// //Добавить кнопку с текстом Orange, при нажатии на которой, цвет страницы становится оранжевым.
+    // document.getElementById("yellowBtn").addEventListener("click", () => {
+    //     document.body.style.backgroundColor = "yellow";
+// })
+//7.4
+setInterval(() => {
+    
+}, 2000);
+window.onload=function(){
+
+    for(let i = 0; i <= 20; i++){
+        el = document.createElement('li');
+        if(i % 2 == 0){
+            el.innerHTML = 'Par';
+        }
+        else{
+            el.innerHTML = 'Impar';
+        }
+        document.getElementById('MyUl').appendChild(el);
+    }
+}
